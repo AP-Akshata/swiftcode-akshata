8d16568eaa0f59ee4a4cfa93a478eacaeda47525
AP-Akshata <akshataph09@gmail.com>
2018-04-14 11:58:03 +0000

    add msglayout

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,18 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
+            "sender": "USER",
+            "text": "Hello"
+	},
+        {
+            'sender': 'BOT',
+            'text': "hi",
+	},
 
+        {
+            'sender': 'BOR',
+            'text': "what"
+                }
+                    ];
 });
