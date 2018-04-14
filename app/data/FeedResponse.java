feee8e8119c74849d388c5ebab0f152c8255ab96
AP-Akshata <akshataph09@gmail.com>
2018-04-14 07:22:09 +0000

    Added newsAgentService

--- app/data/FeedResponse.java
+++ app/data/FeedResponse.java
@@ -1,5 +1,9 @@
 package data;
 
 public class FeedResponse {
+    public String title;
+    public String description;
+    public String pubDate;
+    public String link;
 
 }
--- app/services/FeedService.java
+++ app/services/FeedService.java
@@ -11,5 +11,27 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ExecutionException;
 
 public class FeedService {
+    public FeedResponse getFeedByQuery(String query)
+    {
+        FeedResponse feedResponseObject=new FeedResponse();
+        try{
+            WSRequest feedRequest = WS.url("https://news.google.com/news");
+            CompletionStage<WSResponse> responsePromise = feedRequest
+                    .setQueryParameter("q",query)
+                    .setQueryParameter("output", "rss")
+                    .get();
+            Document feedResponse = responsePromise.thenApply(WSResponse::asXml).toCompletableFuture().get();
+            Node item=feedResponse.getFirstChild().getFirstChild().getChildNodes().item(10);
+            feedResponseObject.title=item.getChildNodes().item(0).getFirstChild().getNodeValue();
+            feedResponseObject.link=item.getChildNodes().item(1).getFirstChild().getNodeValue();
+            feedResponseObject.pubDate=item.getChildNodes().item(3).getFirstChild().getNodeValue();
+            feedResponseObject.description=item.getChildNodes().item(4).getFirstChild().getNodeValue();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return feedResponseObject;
+    }
 
 }
