97f20b18da03fd63ac242dc6ef03a5ec9182cd32
AP-Akshata <akshataph09@gmail.com>
2018-04-14 12:14:16 +0000

    add msglayout

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -11,7 +11,7 @@ app.controller('chatController', function ($scope) {
 	},
 
         {
-            'sender': 'BOR',
+            'sender': 'BOT',
             'text': "what"
                 }
                     ];
